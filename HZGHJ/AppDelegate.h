aabcdb4c05f7b4f6aae7084456ff213f1d3fa69d
Adele2011180073 <1657828388@qq.com>
2016-12-06 15:28:39 +0800

    Initial Commit

---
+++ HZGHJ/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HZGHJ
+//
+//  Created by zhang on 16/12/6.
+//  Copyright © 2016年 FiveFu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
